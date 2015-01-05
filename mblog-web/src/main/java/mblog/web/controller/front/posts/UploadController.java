fd17100bb55f2125fd7e0b655ce9ca228bc998fb
langhsu <langhsu@gmail.com>
2015-01-05 16:49:35 +0800

    上传图片添加类型判断

--- mblog-web/src/main/java/mblog/web/controller/front/posts/UploadController.java
+++ mblog-web/src/main/java/mblog/web/controller/front/posts/UploadController.java
@@ -5,6 +5,10 @@ package mblog.web.controller.front.posts;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import mblog.core.context.AppContext;
 import mblog.web.controller.front.BaseController;
@@ -27,15 +31,36 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController extends BaseController {
 	@Autowired
 	private AppContext appContext;
+
+	private static Map<String, String> errors = new HashMap<String, String>();
+	
+	// 文件允许格式
+	private String[] allowFiles = { ".gif", ".png", ".jpg", ".jpeg", ".bmp" };
+	
+	static {
+		errors.put("SUCCESS", "SUCCESS"); //默认成功
+		errors.put("NOFILE", "未包含文件上传域");
+		errors.put("TYPE", "不允许的文件格式");
+		errors.put("SIZE", "文件大小超出限制");
+		errors.put("REQUEST", "上传请求异常");
+		errors.put("UNKNOWN", "未知错误");
+	}
 	
 	@RequestMapping("/upload")
     public @ResponseBody Data upload(@RequestParam(value = "file", required=false) MultipartFile file,
     		Boolean scale, Integer size) {
     	Data data = Data.failure("error");
     	
-    	if (file.isEmpty()) {
+    	if (file == null || file.isEmpty()) {
+    		data = Data.failure(errors.get("NOFILE"));
 			return data;
 		}
+    	
+    	if (!checkFileType(file.getOriginalFilename())) {
+    		data = Data.failure(errors.get("TYPE"));
+			return data;
+    	}
+    	
     	String root = "/";
     	String realpath = getRealPath(root);
     	try {
@@ -57,9 +82,26 @@ public class UploadController extends BaseController {
 			data = Data.success("", path);
 		} catch (IOException e) {
 			//FIXME: error handle
+			data = Data.failure(errors.get("UNKNOWN"));
 		}
     	
     	return data;
     }
     
+	/**
+	 * 文件类型判断
+	 * 
+	 * @param fileName
+	 * @return
+	 */
+	private boolean checkFileType(String fileName) {
+		Iterator<String> type = Arrays.asList(this.allowFiles).iterator();
+		while (type.hasNext()) {
+			String ext = type.next();
+			if (fileName.toLowerCase().endsWith(ext)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
