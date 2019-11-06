a3b0f790a18339c6349abbd0ea6cc8abf9dc7b19
landy <langxu2@huashenghaoche.com>
2019-11-06 10:02:04 +0800

    新增友情链接模块

--- src/main/java/com/mtons/mblog/modules/template/directive/LinksDirective.java
+++ src/main/java/com/mtons/mblog/modules/template/directive/LinksDirective.java
@@ -7,6 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
+ * Eg:
+ * <@links>
+ *     <#list results as row>
+ * 		<li><a href="${row.url}">${row.name}</a></li>
+ * 	   </#list>
+ * </@links>
+ *
  * @author : landy
  * @version : 1.0
  * @date : 2019/11/6
